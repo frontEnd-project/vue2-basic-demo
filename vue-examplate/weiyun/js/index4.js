2e23329aa74b4c1ccb40c86e410249bf53177bb9
WYseven <consolelog@126.com>
2016-11-22 18:04:33 +0800

    增加vue渲染

--- vue-examplate/weiyun/js/index4.js
+++ vue-examplate/weiyun/js/index4.js
@@ -1,5 +1,11 @@
 
-datas = data.files;
+datas = data.files.map(function (item,index){
+
+    //是否是编辑状态
+    item.isEdtor = false;
+
+    return item;  
+});
 
 var initId = 0;
 
@@ -8,6 +14,14 @@ var initDate = dataAction.getChildsById(datas,initId);
 var vm = new Vue({
     data:{
         fileDate:initDate
+    },
+    methods:{
+        createNewFile(){
+           this.fileDate.unshift({
+                title:"",
+                isEdtor:true
+            }) 
+        }
     }
 });
 
